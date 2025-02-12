0881f23f87487c2525aa41b6fc9c8ca5735aee17
gyubeom <qja0707@gmail.com>
2025-02-12 14:12:00 +0900

    fix: change directory

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ npm install # Required because npm ci will install only prod dependencies becaus
 npm run clean
 npm run build
 
+cp -r /home/site/wwwroot/api/. /home/site/repository/api
 rm -rf /home/site/wwwroot/*
 cp -r /home/site/repository/api/* /home/site/wwwroot/
